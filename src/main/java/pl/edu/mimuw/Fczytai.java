2a5dcaeb6e97820fc8b7243defd6204c886e907e
mg448503 <marcin@giembicki.net>
2023-05-24 14:47:15 +0200

    poprawiam tak zeby dzialalo + literowki

--- src/main/java/pl/edu/mimuw/Fczytai.java
+++ src/main/java/pl/edu/mimuw/Fczytai.java
@@ -3,6 +3,7 @@ package pl.edu.mimuw;
 import java.util.ArrayList;
 
 public class Fczytai {
-    public ArrayList<String> fczytai(){ //wczytuje dane i oddaje je w postaci tablicy stringuf
+    public ArrayList<String> fczytai() { //wczytuje dane i oddaje je w postaci tablicy stringow
+        return null;
     }
 }
