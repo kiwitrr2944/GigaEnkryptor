7e53e210f84c126a76e502c873b4301e3e01cdb0
mg448503 <marcin@giembicki.net>
2023-05-24 14:50:34 +0200

    probujemy utworzyc nowy branch

--- src/main/java/pl/edu/mimuw/Fczytai.java
+++ src/main/java/pl/edu/mimuw/Fczytai.java
@@ -3,6 +3,7 @@ package pl.edu.mimuw;
 import java.util.ArrayList;
 
 public class Fczytai {
+    //tucos sie dzieje
     public ArrayList<String> fczytai() { //wczytuje dane i oddaje je w postaci tablicy stringow
         return null;
     }
