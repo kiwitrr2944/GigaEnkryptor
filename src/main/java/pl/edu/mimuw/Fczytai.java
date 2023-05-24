29a0c25a7da502fe679e4c41ec9f3392c152dbbf
mg448503 <marcin@giembicki.net>
2023-05-24 15:30:03 +0200

    napisał cos elo

--- src/main/java/pl/edu/mimuw/Fczytai.java
+++ src/main/java/pl/edu/mimuw/Fczytai.java
@@ -1,10 +1,51 @@
 package pl.edu.mimuw;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Scanner;
 
 public class Fczytai {
-    //tucos sie dzieje
-    public ArrayList<String> fczytai() { //wczytuje dane i oddaje je w postaci tablicy stringow
-        return null;
+    //tu cos sie dzieje
+    public ArrayList<String> fczytai(String nazwaPliku)  {
+        ArrayList<String> wynik = new ArrayList<>();
+        //wczytuje dane i oddaje je w postaci tablicy stringow
+        try {
+            Scanner skaner = new Scanner(new File(nazwaPliku));
+            while (skaner.hasNext()) {
+                wynik.add(skaner.next());
+            }
+        }
+        catch (FileNotFoundException e) {
+            System.out.println("chyba nei znalazl pliku");
+            e.printStackTrace();
+        }
+        return wynik;
+    }
+
+    public void fpisz(ArrayList<String> zwracanaLista, String nazwaPliku) {
+        //wpisuje do podanego pliku
+        //powinien utworzyc nowy plik, a jesli juz istnial to go usunac i stworzyc nowy
+        try {
+            File plik = new File(nazwaPliku);
+            plik.delete();
+            plik.createNewFile();
+            FileWriter wpisywacz = new FileWriter(plik);
+            Iterator<String> it = zwracanaLista.iterator();
+            while (it.hasNext()) {
+                wpisywacz.write(it.next() + " ");
+            }
+            wpisywacz.close();
+        }
+        catch (FileNotFoundException e) {
+            System.out.println("chyba nei znalazl pliku");
+            e.printStackTrace();
+        } catch (IOException e) {
+            System.out.println("eeee cos nie dziala");
+            e.printStackTrace();
+        }
     }
 }
