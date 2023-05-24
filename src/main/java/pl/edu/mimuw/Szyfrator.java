5438ded860118dbc84c34cf8a678ec1f52f66dab
kiwitrr2944 <wiktorr2112@gmail.com>
2023-05-24 15:01:45 +0200

    Dodanie szyfrowanie Cezarem

--- src/main/java/pl/edu/mimuw/Szyfrator.java
+++ src/main/java/pl/edu/mimuw/Szyfrator.java
@@ -1,7 +1,23 @@
 package pl.edu.mimuw;
 
 public class Szyfrator {
-    static public String szyfróiCezar() { return null; }
+    static public String szyfróiCezar(String napis, int pszesuniencie) {
+        String wynik = new String();
+        for (int i=0; i<napis.length(); ++i) {
+            int kod = (int) napis.charAt(i);
+            int baza = 65;
+            if (kod >= 97) {
+                baza = 97;
+            }
+            kod -= baza;
+            kod += pszesuniencie;
+            kod %= 26;
+            kod += baza;
+            char x = (char) kod;
+            wynik += x;
+        }
+        return wynik;
+    }
     static public String deSzyfróiCezar() { return null; }
     static public String szyfróiMors() { return null; }
     static public String deSzyfróiMors() { return null; }
