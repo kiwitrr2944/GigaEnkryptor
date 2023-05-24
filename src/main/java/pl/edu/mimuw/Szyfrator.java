dbae41743f71dfc701c5ebb70160eed5823ce315
ReadHegel <sobotafran@gmail.com>
2023-05-24 14:46:41 +0200

    Zadanie dla Wiktora zaimplepientuj te cztery funkcje

---
+++ src/main/java/pl/edu/mimuw/Szyfrator.java
@@ -0,0 +1,9 @@
+package pl.edu.mimuw;
+
+public class Szyfrator {
+    static public String szyfróiCezar() {}
+    static public String deSzyfróiCezar() {}
+    static public String szyfróiMors() {}
+    static public String deSzyfróiMors() {}
+
+}
