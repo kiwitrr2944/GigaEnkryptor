9ccd57f826e0e0f1a624ee6e90642f27b3201d3b
ReadHegel <sobotafran@gmail.com>
2023-05-24 15:17:56 +0200

    poprawiłem wywołania funkcji cezaraz

--- src/main/java/pl/edu/mimuw/Main.java
+++ src/main/java/pl/edu/mimuw/Main.java
@@ -4,13 +4,14 @@ public class Main {
     //args[0] - plik; args[1] - jaki jest wejsciowy {0, 1, 2}; args[2] jaki jest wyjsciowy;
     // args[3] - nazwa wyjsciowego
     public static void main(String[] args) {
+        int pszesóniencie = 69;
         var myFczytai = new Fczytai();
         var mySzyfrator = new Szyfrator();
         var sluwka = myFczytai.fczytai(args[0]);
         if(args[1] == "1") {
             for(int i=0; i<sluwka.size(); i++) {
                 var wyras = sluwka.get(i);
-                sluwka.set(i, Szyfrator.deSzyfróiCezar(wyras));
+                sluwka.set(i, Szyfrator.deSzyfróiCezar(wyras, pszesóniencie));
             }
         }
         if(args[1] == "2") {
@@ -23,7 +24,7 @@ public class Main {
         if(args[2] == "1") {
             for(int i=0; i<sluwka.size(); i++) {
                 var wyras = sluwka.get(i);
-                sluwka.set(i, Szyfrator.szyfróiCezar(wyras));
+                sluwka.set(i, Szyfrator.szyfróiCezar(wyras, pszesóniencie));
             }
         }
         if(args[2] == "2") {
