eab224fea2ccf2a2926c3da7dcdd4cc8788ee58b
kiwitrr2944 <wiktorr2112@gmail.com>
2023-05-24 15:28:20 +0200

    szyfr

--- src/main/java/pl/edu/mimuw/Szyfrator.java
+++ src/main/java/pl/edu/mimuw/Szyfrator.java
@@ -1,9 +1,70 @@
 package pl.edu.mimuw;
 
+import com.sun.nio.sctp.AbstractNotificationHandler;
+
 import java.util.HashMap;
+import java.util.Map;
 
 public class Szyfrator {
-    public HashMap <Character, String>
+    public HashMap<Character, String> morse;
+    public HashMap<String, Character> esrom;
+    Szyfrator () {
+        morse = new HashMap<Character, String>();
+        esrom = new HashMap<String, Character>()
+        morse.put('a', ".-");
+        morse.put('b', "-...");
+        morse.put('c',  "-.-");
+        morse.put('d',  "-..");
+        morse.put('e',    ".");
+        morse.put('f', "..-.");
+        morse.put('g',  "--.");
+        morse.put('h', "....");
+        morse.put('i',   "..");
+        morse.put('j', ".---");
+        morse.put('k',   "-.");
+        morse.put('l', ".-..");
+        morse.put('m',   "--");
+        morse.put('n',   "-.");
+        morse.put('o',  "---");
+        morse.put('p', ".--.");
+        morse.put('q', "--.-");
+        morse.put('r', ".-.");
+        morse.put('s',  "...");
+        morse.put('t',   "-");
+        morse.put('u',  "..-");
+        morse.put('v', "...-");
+        morse.put('w',  ".--");
+        morse.put('x', "-..-");
+        morse.put('y', "-.--");
+        morse.put('z', "--..");
+
+        esrom.put(".-", 'a');
+        esrom.put("-...", 'b');
+        esrom.put("-.-.", 'c');
+        esrom.put("-..", 'd');
+        esrom.put(".", 'e');
+        esrom.put("..-.", 'f');
+        esrom.put("--.", 'g');
+        esrom.put("....", 'h');
+        esrom.put("..", 'i');
+        esrom.put(".---", 'j');
+        esrom.put("-.-", 'k');
+        esrom.put(".-..", 'l');
+        esrom.put("--", 'm');
+        esrom.put("-.", 'n');
+        esrom.put("---", 'o');
+        esrom.put(".--.", 'p');
+        esrom.put("--.-", 'q');
+        esrom.put(".-.", 'r');
+        esrom.put("...", 's');
+        esrom.put("-", 't');
+        esrom.put("..-", 'u');
+        esrom.put("...-", 'v');
+        esrom.put(".--", 'w');
+        esrom.put("-..-", 'x');
+        esrom.put("-.--", 'y');
+        esrom.put("--..", 'z');
+    }
     static public String szyfróiCezar(String napis, int pszesuniencie) {
         String wynik = new String();
         for (int i=0; i<napis.length(); ++i) {
